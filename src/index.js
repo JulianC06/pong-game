01d08d146c63a490702955015cb453c100e028cf
JulianC06 <87028380+JulianC06@users.noreply.github.com>
2022-05-15 14:07:30 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,1 @@
-const config = {
-    type: Phaser.AUTO,
-    parent: 'game-pong',
-    with:800,
-    height:600,
-    scale:{
-        mode: Phaser.Scale.RESIZE,
-        autoCenter: Phaser.Scale.CENTER_BOTH
-    },
-    physics:{
-        default: 'arcade',
-        arcade:{
-            gravity:false
-        }
-    }
-};
 
-
-const game = new Phaser.Game(config);
